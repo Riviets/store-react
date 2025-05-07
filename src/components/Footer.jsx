e7fdd0515e5686971c578a90af684b7c1df3fb44
Riviets <vladvladpobuta@gmail.com>
2025-05-07 21:59:12 +0300

    Implemented footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { footerItems } from "../constants";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-zinc-900 text-white text-center">Footer</div>
+    <div className="px-5 w-full bg-zinc-900 text-white  py-6">
+      <div className="container flex justify-between items-center">
+        <p className="font-medium">© Copyrighted 2025</p>
+        <div className="flex gap-5">
+          {footerItems.map((item) => (
+            <a
+              key={item.path}
+              href={item.path}
+              target="_blank"
+              className="p-3 rounded-lg bg-zinc-800 border-1 border-white-50 hover:scale-105 transition-all duration-300"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,17 @@
+import { instaIcon } from "../assets/icons/socials/insta";
+import { linkedinIcon } from "../assets/icons/socials/linkedin";
+import { facebookIcon } from "../assets/icons/socials/facebook";
+
 export const API_URL = "https://fakestoreapi.com";
 
 export const navbarItems = [
   { text: "About", path: "/about" },
   { text: "Shop", path: "/shop" },
   { text: "Profile", path: "/profile" },
+];
+
+export const footerItems = [
+  { icon: instaIcon, path: "https://instagram.com" },
+  { icon: linkedinIcon, path: "https://linkedin.com" },
+  { icon: facebookIcon, path: "https://facebook.com" },
 ];
